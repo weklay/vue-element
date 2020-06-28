7fc24416f2802b3d326ea554e40d81821daf6b22
zengzhi <zengz@doordu.com>
2020-06-28 16:21:01 +0800

    修复二级目录文件丢失

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ const BASE_API = process.env.VUE_APP_BASE_API
 const IS_PROD = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-  publicPath: '/', //根路径
+  publicPath: './', //根路径
   outputDir: 'dist',  //构建输出目录
   lintOnSave: IS_PROD, //是否开启eslint保存检测，有效值：true || false || 'error'
   productionSourceMap: false,
